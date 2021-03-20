24e0ad42a3aa6d96002a306df9a1993f3aec6732
Zhaiyu Chen <24312770+chenzhaiyu@users.noreply.github.com>
2021-03-20 20:17:00 +0100

    Training p2s_max model by default

--- p2s/full_train.py
+++ p2s/full_train.py
@@ -9,75 +9,37 @@ import os
 # see https://forums.fast.ai/t/understanding-code-error-expected-more-than-1-value-per-channel-when-training/9257/12
 
 
-def full_train(opt):
-
-    points_to_surf_train.points_to_surf_train(opt)
-
-
 if __name__ == '__main__':
 
-    model_name = 'debug'
-    dataset = 'helsinki_noise_0.001'
-    base_dir = '../datasets'
-    in_dir_train = os.path.join(base_dir, dataset)
-
-    train_set = 'trainset.txt'
-    val_set = 'valset.txt'
-    test_set = 'testset.txt'
-
-    # features = ['imp_surf', 'patch_pts_ids', 'p_index']  # l2-loss
-    features = ['imp_surf_magnitude', 'imp_surf_sign', 'patch_pts_ids', 'p_index']  # l2-loss + BCE-loss
-
-    # workers = 22  # for strong training machine
-    workers = 7  # for typical PC
-
-    # batch_size = 501  # ~7.5 GB memory on 4 2080 TI for 300 patch points + 1000 sub-sample points
-    # batch_size = 3001  # ~10 GB memory on 4 2080 TI for 50 patch points + 200 sub-sample points
-    batch_size = 50  # ~7 GB memory on 1 1070 for 300 patch points + 1000 sub-sample points
-
-    # grid_resolution = 256  # quality like in the paper
-    grid_resolution = 128  # quality for a short test
-    rec_epsilon = 3
-    certainty_threshold = 13
-    sigma = 5
-
-    fixed_radius = False
-    patch_radius = 0.1 if fixed_radius else 0.0
-    single_transformer = 0
-    shared_transformer = 0
-
-    # os.environ["CUDA_VISIBLE_DEVICES"] = "0"
-
+    # settings for training p2s_max model
     train_params = [
-        '--name', model_name,
-        '--desc', model_name,
-        '--indir', in_dir_train,
+        '--name', 'debug',
+        '--indir', '../datasets/abc_train',
+        '--testset', 'valset.txt',
         '--outdir', 'models',
-        '--trainset', train_set,
-        '--testset', val_set,
-        '--net_size', str(1024),
         '--nepoch', str(10),
         '--lr', str(0.01),
+        '--scheduler_steps', str(100), str(200),
         '--debug', str(0),
-        '--workers', str(workers),
-        '--batchSize', str(batch_size),
+        '--workers', str(7),
+        '--batchSize', str(101),
         '--points_per_patch', str(300),
         '--patches_per_shape', str(1000),
         '--sub_sample_size', str(1000),
-        '--cache_capacity', str(10),
-        '--patch_radius', str(patch_radius),
-        '--single_transformer', str(single_transformer),
-        '--shared_transformer', str(shared_transformer),
+        '--cache_capacity', str(30),
+        '--patch_radius', str(0.0),
+        '--single_transformer', str(0),
+        '--shared_transformer', str(0),
+        '--uniform_subsample', str(1),
+        '--use_point_stn', str(0),
+        '--net_size', str(1024),
         '--patch_center', 'mean',
         '--training_order', 'random_shape_consecutive',
-        '--use_point_stn', str(1),
-        '--uniform_subsample', str(0),
-        '--outputs',
-    ]
-    train_params += features
+        '--outputs', 'imp_surf_magnitude', 'imp_surf_sign', 'patch_pts_ids', 'p_index'
+        ]
 
     # train model on GT data with multiple query points per patch
     train_opt = points_to_surf_train.parse_arguments(train_params)
-    full_train(train_opt)
+    points_to_surf_train.points_to_surf_train(train_opt)
     
     print('MeshNet training is finished!')
